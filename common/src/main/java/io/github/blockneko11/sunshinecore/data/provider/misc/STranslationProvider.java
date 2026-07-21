9361a8d70ea5f239ee92754f7a73adfea5a95e21
BlockNeko-11 <block_mrlimr267@outlook.com>
2026-07-22 02:00:21 +0800

    fix (datagen): STranslationProvider throws ISE unexpectedly
    when adding translations of a Block and a ItemNameBlockItem
    
    also renamed some methods of STranslationProvider

--- common/src/main/java/io/github/blockneko11/sunshinecore/data/provider/misc/STranslationProvider.java
+++ common/src/main/java/io/github/blockneko11/sunshinecore/data/provider/misc/STranslationProvider.java
@@ -6,7 +6,6 @@ import net.minecraft.data.CachedOutput;
 import net.minecraft.data.DataProvider;
 import net.minecraft.data.PackOutput;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.block.Block;
 import org.jetbrains.annotations.NotNull;
@@ -69,7 +68,11 @@ public abstract class STranslationProvider implements DataProvider {
     public interface Translator {
         void add(String key, String value);
 
-        default void add(ItemLike item, String value) {
+        default void addBlock(Block block, String value) {
+            this.add(block.getDescriptionId(), value);
+        }
+
+        default void addItem(ItemLike item, String value) {
             this.add(item.asItem().getDescriptionId(), value);
         }
 
